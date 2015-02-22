f18a836e084a3d4072ce667362689c6e6b8b1c24
jtuttas <tuttas@mmbbs.de>
2015-02-22 16:21:50 +0100

    neue Server Adresse

--- web/js/main.js
+++ web/js/main.js
@@ -7,7 +7,7 @@ var webSocket;
 
 $(document).ready( function() {
      //webSocket = new WebSocket("ws://service.joerg-tuttas.de:8082/ChatWebApplication/chat");
-     webSocket = new WebSocket("ws://raspi.joerg-tuttas.de:8081/ChatWebApplication/chat");
+     webSocket = new WebSocket("ws://service.joerg-tuttas.de:8081/ChatWebApplication/chat");
      //webSocket = new WebSocket("ws://localhost:8080/ChatWebApplication/chat");
      
      webSocket.onmessage = function(event){
