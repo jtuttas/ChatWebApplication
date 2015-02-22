131fae38a057c8b86aa686eab47d54beb0180299
jtuttas <tuttas@mmbbs.de>
2015-02-22 12:06:07 +0100

    erstes element wird angefügt

--- web/js/main.js
+++ web/js/main.js
@@ -6,10 +6,12 @@
 var webSocket;
 
 $(document).ready( function() {
-     webSocket = new WebSocket("ws://service.joerg-tuttas.de:8082/ChatWebApplication/chat");
+     //webSocket = new WebSocket("ws://service.joerg-tuttas.de:8082/ChatWebApplication/chat");
+     webSocket = new WebSocket("ws://raspi.joerg-tuttas.de:8081/ChatWebApplication/chat");
+     //webSocket = new WebSocket("ws://localhost:8080/ChatWebApplication/chat");
      
      webSocket.onmessage = function(event){
-                    $("#receive").append("<p>"+event.data+"</p>");
+                    $("#receive").prepend("<p>"+event.data+"</p>");
                 };
     });
 
